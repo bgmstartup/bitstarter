fbd538e0fcc716b789fa7573a9ab0c4acbe2e80c
Ubuntu <ubuntu@ip-172-31-37-23.us-west-2.compute.internal>
2013-07-10 08:43:32 +0000

    white space

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
 	fs.readFile('index.html', function (err, data){
 		if (err) throw err;
 	
- 		 response.send(data.toString());
+	 		 response.send(data.toString());
 	});
 });
 
